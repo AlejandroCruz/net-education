1f1176cc3323b4d9b85094edcf386fb9a3d9e482
Alejandro Cruz <acdeveloper@outlook.com>
2018-05-27 21:29:10 -0400

    Still at multi chart for Dashboard

--- netedu-app/src/components/Dashboard/DashboardContent.vue
+++ netedu-app/src/components/Dashboard/DashboardContent.vue
@@ -5,7 +5,7 @@
       <div class="col-md">
         <div class="card">
           <div class="card-body" v-for="item in charts" :key="item.id">
-            <h5 class="card-title">{{ item.Title }}</h5>
+            <h5 class="card-title" v-show="waitForChart(item.id, item.cData)">Title</h5>
             <canvas id="c1-chart"/>
             <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
             <a href="#" class="btn btn-primary">Go somewhere</a>
@@ -36,26 +36,45 @@ export default {
   data () {
     return {
       charts: [
-        { id: 1, cData: chartData.c1Data, cTitle: 'Test pending' },
-        { id: 2, cData: chartData.c2Data, cTitle: 'Practice Test' }
+        { id: 'c1-chart', cData: chartData.c1Data, cTitle: 'Test pending' }
+        // { id: 'c2-chart', cData: chartData.c2Data, cTitle: 'Practice Test' }
       ]
     }
   },
   components: {
   },
-  mounted () {
-    this.createChart('c1-chart', this.charts[0].cData)
-    this.createChart('c2-chart', this.charts[1].cData)
-  },
+  // mounted () {
+  //   this.createChart('c1-chart', this.charts[0].cData)
+  //   this.createChart('c2-chart', this.charts[1].cData)
+  // },
   methods: {
+    waitForChart (itemID, itemCData) {
+      this.createChart(itemID, itemCData).then(function (r) {
+        return r
+      })
+    },
     createChart (cId, cData) {
       const cElement = document.getElementById(cId)
-      // eslint-disable-next-line
-      const myChart = new Chart(cElement, {
-        type: cData.type,
-        data: cData.data,
-        options: cData.options
+
+      return new Promise((resolve) => {
+        // eslint-disable-next-line
+        const myChart = new Chart(cElement, {
+          type: cData.type,
+          data: cData.data,
+          options: cData.options
+        })
+        resolve(true)
       })
+
+      // console.log('DashboardContent>createChart --> document.getElementById(cId)')
+      // console.log(cElement)
+      // console.log('<--')
+
+      // const myChart = new Chart(cElement, {
+      //   type: cData.type,
+      //   data: cData.data,
+      //   options: cData.options
+      // })
     }
   }
 }
